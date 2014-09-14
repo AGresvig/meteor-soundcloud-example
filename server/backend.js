ce6ce427a26ef276394cd8fa7c4d30b590fb96de
AGresvig <aksel@agresvig.com>
2014-09-14 08:54:23 +0000

    Integrated with Soundcloud API

--- server/backend.js
+++ server/backend.js
@@ -1,3 +1,13 @@
+// first, remove configuration entry in case service is already configured
+ServiceConfiguration.configurations.remove({
+  service: "soundcloud"
+});
+ServiceConfiguration.configurations.insert({
+  service: "soundcloud",
+  clientId: process.env.SC_CLIENTID,
+  secret: process.env.SC_SECRET
+});
+
 Meteor.startup(function () {
     // code to run on server at startup
 });
