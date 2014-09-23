4ce3c699f8b34f25ff18eec1c24924616c96461b
AGresvig <aksel@agresvig.com>
2014-09-23 10:16:58 +0000

    Added soundcloud OAuth login

--- client/meteor-music-blog.js
+++ client/meteor-music-blog.js
@@ -12,5 +12,15 @@ Template.hello.events({
         // increment the counter when button is clicked
         Session.set("counter", Session.get("counter") + 1);
     }
-  });
-}
+});
+
+Template._loginButtonsLoggedInDropdown.events({
+    'click #soundcloud-login': function(event) {
+        event.stopPropagation();
+        Template._loginButtons.toggleDropdown();
+        Meteor.loginWithSoundcloud(function(evt) {
+            console.log("Logged in. Evt below");
+            console.log(evt);
+        });
+    }
+});
