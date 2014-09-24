0f6e34301c1578a64140c4af6355ebe02ed8b7d4
Aksel Gresvig <aksel@agresvig.com>
2014-09-24 17:34:41 +0200

    Added functionality to select tracks

--- client/meteor-music-blog.js
+++ client/meteor-music-blog.js
@@ -18,3 +18,25 @@ Template._loginButtonsLoggedInDropdown.events({
 Template.importLikes.likes = function() {
     return Soundcloud.getLikes();
 }
+
+Template.importLikes.events({
+    'click .track': function (event) {
+        var track = this;
+        var selectedTracks = Session.get("selectedTracks") ? Session.get("selectedTracks").slice() : [];
+
+        //Meteor._debug("Removing tag \"" + this + "\", index: " + index, ", typeof(this) = " + typeof(this).toString());
+
+        //If we have this track selected already
+        if(_.contains(selectedTracks, track)) {
+            //..then unselect it
+            track.selected = false;
+            Session.set("selectedTracks", _.without(selectedTracks, track));
+        } else {
+            //..otherwise select it
+            track.selected = true;
+            Session.set("selectedTracks", selectedTracks.concat(track));
+        }
+        //Toggle 'active' css class on the list-group item for styling 
+        $(event.target).closest('.track').toggleClass('active')
+    }
+});
